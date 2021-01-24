5742fcb373cac1d5ab843ff3ba4aa4dea9d87290
fdlucky <30947710+fdlucky@users.noreply.github.com>
2021-01-25 06:10:53 +0800

    update

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-1_4hWFlYko5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5nj07h5imuq2445fp5f6op7438', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
